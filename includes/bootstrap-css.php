14277fcef5c6bba8d8b9a614fc8802e2b1640daa
ivonilsoncardoso@hotmail.com <ivonilsoncardoso@hotmail.com>
2022-03-02 17:44:23 -0300

    atualização para bootstrap 4

---
+++ includes/bootstrap-css.php
@@ -0,0 +1,8 @@
+	<!--<link rel="stylesheet" type="text/css" href="../css/bootstrap2.min.css">-->
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../bibliotecas/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../bibliotecas/datatables/dataTables.bootstrap4.css">
+	<link rel="stylesheet" type="text/css" href="../css/sb-admin.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/sb-admin.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/abg.css">
+	<script src="../js/abg.js"></script>
