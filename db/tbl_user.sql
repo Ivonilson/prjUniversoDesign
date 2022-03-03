6d6f4a13cb6da551185190d5a5d5f2b4e7e60eb8
Ivonilson <ivonilsoncardoso@hotmail.com>
2022-03-02 22:12:11 -0300

    limpar

--- db/tbl_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE table tbl_user(
-	
-	id_user int(8) PRIMARY KEY AUTO_INCREMENT,
-	user varchar (15) UNIQUE,
-	pass varchar (8) UNIQUE
-
-) engine = innoDB;
--- includes/rodape.php
+++
@@ -1,8 +0,0 @@
-<!-- rodapé --->
-<footer  class="sticky-footer">
-	<div class="container">
-		<div class="text-center">
-			<small>&copy;AbgSoluções -  <?=date("Y")?> - abgsolucoes.tec.br </small>
-		</div>
-	</div>
-</footer>
