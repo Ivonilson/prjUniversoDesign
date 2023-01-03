b8566ac877544403215decfe0507f5ab7bdc8851
Ivonilson <ivonilsoncardoso@hotmail.com>
2023-01-02 22:23:21 -0300

    update

--- model/phpMailer.php
+++ model/phpMailer.php
@@ -14,11 +14,11 @@ require"../phpMailer/src/SMTP.php";
       $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
       $mail->SMTPAuth = true; // authentication enabled
       $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-      $mail->Host = "br474.hostgator.com.br";
+      $mail->Host = ";
       $mail->Port = 465; // or 587
       $mail->IsHTML(true);
-      $mail->Username = "universodesign@universodesigncv.com.br";
-      $mail->Password = "#F2023Universo";
+      $mail->Username = "";
+      $mail->Password = "";
       $mail->SetFrom("universodesign@universodesigncv.com.br", "Universo Design - Site");
       $mail->Subject = utf8_decode($assunto);
       $mail->Body = utf8_decode($mensagem);
